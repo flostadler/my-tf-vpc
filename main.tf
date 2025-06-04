e6363308b0934c8a643444421aa80d3c97eeccc7
Florian Stadler <florian@pulumi.com>
2025-06-04 20:50:08 +0200

    Change input types

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ data "aws_availability_zones" "available" {
 
 locals {
   # Get the first N AZs
-  azs = slice(data.aws_availability_zones.available.names, 0, var.num_azs)
+  azs = slice(data.aws_availability_zones.available.names, 0, tonumber(var.num_azs))
 
   # Calculate the number of bits needed for the subnet mask
   vpc_cidr_bits = tonumber(split("/", var.cidr)[1])
   num_azs       = length(local.azs)
 
   # Calculate the number of IPs per subnet
-  private_ips_per_subnet = pow(2, 32 - var.private_subnet_size)
-  public_ips_per_subnet  = pow(2, 32 - var.public_subnet_size)
+  private_ips_per_subnet = pow(2, 32 - tonumber(var.private_subnet_size))
+  public_ips_per_subnet  = pow(2, 32 - tonumber(var.public_subnet_size))
 
   # Calculate the number of IPs needed for all subnets
   total_ips_needed = (private_ips_per_subnet + public_ips_per_subnet) * local.num_azs
@@ -23,7 +23,7 @@ locals {
   base_ip = cidrhost(var.cidr, 0)
 
   # Calculate how many bits we need for private subnets
-  private_bits_needed = ceil(log(local.num_azs * pow(2, var.private_subnet_size - local.vpc_cidr_bits), 2))
+  private_bits_needed = ceil(log(local.num_azs * pow(2, tonumber(var.private_subnet_size) - local.vpc_cidr_bits), 2))
 
   # Split the VPC CIDR - first part for private, rest for public
   private_range = cidrsubnet(var.cidr, private_bits_needed, 0)
@@ -32,13 +32,13 @@ locals {
   # Generate private subnet CIDRs
   private_subnets = [
     for i in range(local.num_azs) :
-    cidrsubnet(private_range, var.private_subnet_size - (local.vpc_cidr_bits + private_bits_needed), i)
+    cidrsubnet(private_range, tonumber(var.private_subnet_size) - (local.vpc_cidr_bits + private_bits_needed), i)
   ]
 
   # Generate public subnet CIDRs
   public_subnets = [
     for i in range(local.num_azs) :
-    cidrsubnet(public_range, var.public_subnet_size - (local.vpc_cidr_bits + private_bits_needed), i)
+    cidrsubnet(public_range, tonumber(var.public_subnet_size) - (local.vpc_cidr_bits + private_bits_needed), i)
   ]
 }
 
--- variables.tf
+++ variables.tf
@@ -11,30 +11,18 @@ variable "cidr" {
 
 variable "num_azs" {
   description = "Number of AZs to use"
-  type        = number
-  default     = 3
-  validation {
-    condition     = var.num_azs >= 1 && var.num_azs <= 6
-    error_message = "Number of AZs must be between 1 and 6."
-  }
+  type        = string
+  default     = "3"
 }
 
 variable "private_subnet_size" {
   description = "The size of each private subnet in bits (e.g., 20 for /20)"
-  type        = number
-  default     = 20
-  validation {
-    condition     = var.private_subnet_size >= 16 && var.private_subnet_size <= 28
-    error_message = "Private subnet size must be between 16 and 28 bits."
-  }
+  type        = string
+  default     = "20"
 }
 
 variable "public_subnet_size" {
   description = "The size of each public subnet in bits (e.g., 24 for /24)"
-  type        = number
-  default     = 24
-  validation {
-    condition     = var.public_subnet_size >= 16 && var.public_subnet_size <= 28
-    error_message = "Public subnet size must be between 16 and 28 bits."
-  }
+  type        = string
+  default     = "24"
 }
